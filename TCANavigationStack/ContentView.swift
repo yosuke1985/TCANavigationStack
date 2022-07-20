a15b1b42b758077aeb9a26ae26384e01c7fa1c1b
Yosuke NAKAYAMA <ihatenonoildressing@gmail.com>
2022-07-20 09:33:44 +0900

    Initial Commit

---
+++ TCANavigationStack/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TCANavigationStack
+//
+//  Created by Yosuke NAKAYAMA on 2022/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCANavigationStack/TCANavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCANavigationStackApp.swift
+//  TCANavigationStack
+//
+//  Created by Yosuke NAKAYAMA on 2022/07/20.
+//
+
+import SwiftUI
+
+@main
+struct TCANavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
